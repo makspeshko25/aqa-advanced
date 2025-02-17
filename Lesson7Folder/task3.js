a558b77595406ca44e90b353ebde4f3250d89606
Maksym Peshko <makspeshko@gmail.com>
2025-02-17 17:43:20 +0200

    Updated task3 as per comments

--- Lesson7Folder/task3.js
+++ Lesson7Folder/task3.js
@@ -1,18 +1,18 @@
-function divide(numerator,denominator){
-    try{
-    if(denominator===0){
-        throw new Error("you can't divide by 0");
-    } if(typeof denominator!=="number"||typeof numerator!=="number"){
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error("You can't divide by 0");
+    }
+    if (typeof numerator !== "number" || typeof denominator !== "number") {
         throw new Error("Invalid data type");
     }
-    const result=numerator/denominator;
-    return result;
+    return numerator / denominator;
+}
+
+try {
+    let result = divide(10, 0);
+    console.log("Result:", result);
 } catch (error) {
     console.log("Error:", error.message);
-    return null;
 } finally {
     console.log("Execution completed.");
 }
-}
-
-console.log(divide(1,0))
